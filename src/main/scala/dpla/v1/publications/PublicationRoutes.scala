619ac8136769e9e16f6be2606200f0406863c0e9
AudreyAltman <audreykaltman@gmail.com>
2022-01-19 14:04:48 -0500

    add version to route

--- src/main/scala/dpla/v1/publications/PublicationRoutes.scala
+++ src/main/scala/dpla/v1/publications/PublicationRoutes.scala
@@ -22,79 +22,120 @@ class PublicationRoutes(elasticSearchClient: ElasticSearchClient)(implicit val s
   // needed for the future map/onComplete
   implicit val executionContext: ExecutionContextExecutor = system.executionContext
 
-  val publicationRoutes: Route =
-    pathPrefix("ebooks") {
+  val publicationRoutes: Route = {
+    pathPrefix("v1") {
       concat(
-        pathEnd {
-          get {
-            parameters(
-              "dataProvider".optional,
-              "exact_field_match".optional,
-              "facets".optional,
-              "facet_size".optional,
-              "isShownAt".optional,
-              "object".optional,
-              "page".optional,
-              "page_size".optional,
-              "q".optional,
-              "sourceResource.creator".optional,
-              "sourceResource.date.displayDate".optional,
-              "sourceResource.description".optional,
-              "sourceResource.format".optional,
-              "sourceResource.language.name".optional,
-              "sourceResource.publisher".optional,
-              "sourceResource.subject.name".optional,
-              "sourceResource.subtitle".optional,
-              "sourceResource.title".optional) {
+        pathPrefix("ebooks") {
+          concat(
+            pathEnd {
+              get {
+                parameters(
+                  "dataProvider".optional,
+                  "exact_field_match".optional,
+                  "facets".optional,
+                  "facet_size".optional,
+                  "isShownAt".optional,
+                  "object".optional,
+                  "page".optional,
+                  "page_size".optional,
+                  "q".optional,
+                  "sourceResource.creator".optional,
+                  "sourceResource.date.displayDate".optional,
+                  "sourceResource.description".optional,
+                  "sourceResource.format".optional,
+                  "sourceResource.language.name".optional,
+                  "sourceResource.publisher".optional,
+                  "sourceResource.subject.name".optional,
+                  "sourceResource.subtitle".optional,
+                  "sourceResource.title".optional) {
 
-              (dataProvider,
-               exactFieldMatch,
-               facets,
-               facetSize,
-               isShownAt,
-              `object`,
-               page,
-               pageSize,
-               q,
-               creator,
-               date,
-               description,
-               format,
-               language,
-               publisher,
-               subject,
-               subtitle,
-               title) =>
+                  (dataProvider,
+                   exactFieldMatch,
+                   facets,
+                   facetSize,
+                   isShownAt,
+                   `object`,
+                   page,
+                   pageSize,
+                   q,
+                   creator,
+                   date,
+                   description,
+                   format,
+                   language,
+                   publisher,
+                   subject,
+                   subtitle,
+                   title) =>
 
-              val rawParams = RawParams(
-                dataProvider=dataProvider,
-                exactFieldMatch=exactFieldMatch,
-                facets=facets,
-                facetSize=facetSize,
-                isShownAt=isShownAt,
-                `object`=`object`,
-                page=page,
-                pageSize=pageSize,
-                q=q,
-                creator=creator,
-                date=date,
-                description=description,
-                format=format,
-                language=language,
-                publisher=publisher,
-                subject=subject,
-                subtitle=subtitle,
-                title=title
-              )
+                    val rawParams = RawParams(
+                      dataProvider = dataProvider,
+                      exactFieldMatch = exactFieldMatch,
+                      facets = facets,
+                      facetSize = facetSize,
+                      isShownAt = isShownAt,
+                      `object` = `object`,
+                      page = page,
+                      pageSize = pageSize,
+                      q = q,
+                      creator = creator,
+                      date = date,
+                      description = description,
+                      format = format,
+                      language = language,
+                      publisher = publisher,
+                      subject = subject,
+                      subtitle = subtitle,
+                      title = title
+                    )
 
-              ParamValidator.getSearchParams(rawParams) match {
-                case Success(validParams) =>
-                  onComplete(elasticSearchClient.search(validParams)) {
+                    ParamValidator.getSearchParams(rawParams) match {
+                      case Success(validParams) =>
+                        onComplete(elasticSearchClient.search(validParams)) {
+                          case Success(response) => response match {
+                            case Right(pubsFuture) =>
+                              onComplete(pubsFuture) {
+                                case Success(pubs) =>
+                                  complete(pubs)
+                                case Failure(e) =>
+                                  // Failure to parse ElasticSearch response
+                                  System.out.println(s"Error: $e")
+                                  complete(InternalServerError)
+                              }
+                            case Left(status) =>
+                              // ElasticSearch returned an error
+                              val code = status.value
+                              val msg = status.reason
+                              System.out.println(s"Error: $code: $msg")
+                              complete(InternalServerError)
+                          }
+                          case Failure(e) =>
+                            // The call to the ElasticSearch API failed
+                            System.out.println(e)
+                            complete(InternalServerError)
+                        }
+                      case Failure(e: ValidationException) =>
+                        // The user submitted invalid parameters
+                        System.out.println(e.getMessage)
+                        complete(HttpResponse(BadRequest, entity = e.getMessage))
+                      case Failure(e) =>
+                        // This shouldn't happen
+                        System.out.println(e.getMessage)
+                        complete(HttpResponse(InternalServerError, entity = e.getMessage))
+                    }
+                }
+              }
+            },
+            path(Segment) { id =>
+              get {
+                rejectEmptyResponse {
+
+                  onComplete(elasticSearchClient.fetch(id)) {
                     case Success(response) => response match {
-                      case Right(pubsFuture) =>
-                        onComplete(pubsFuture) {
-                          case Success(pubs) =>
-                            complete(pubs)
+                      case Right(pubFuture) =>
+                        onComplete(pubFuture) {
+                          case Success(pub) =>
+                            complete(pub)
                           case Failure(e) =>
                             // Failure to parse ElasticSearch response
                             System.out.println(s"Error: $e")
@@ -112,48 +153,12 @@ class PublicationRoutes(elasticSearchClient: ElasticSearchClient)(implicit val s
                       System.out.println(e)
                       complete(InternalServerError)
                   }
-                case Failure(e: ValidationException) =>
-                  // The user submitted invalid parameters
-                  System.out.println(e.getMessage)
-                  complete(HttpResponse(BadRequest, entity = e.getMessage))
-                case Failure(e) =>
-                  // This shouldn't happen
-                  System.out.println(e.getMessage)
-                  complete(HttpResponse(InternalServerError, entity = e.getMessage))
-              }
-            }
-          }
-        },
-        path(Segment) { id =>
-          get {
-            rejectEmptyResponse {
-
-              onComplete(elasticSearchClient.fetch(id)) {
-                case Success(response) => response match {
-                  case Right(pubFuture) =>
-                    onComplete(pubFuture) {
-                      case Success(pub) =>
-                        complete(pub)
-                      case Failure(e) =>
-                        // Failure to parse ElasticSearch response
-                        System.out.println(s"Error: $e")
-                        complete(InternalServerError)
-                    }
-                  case Left(status) =>
-                    // ElasticSearch returned an error
-                    val code = status.value
-                    val msg = status.reason
-                    System.out.println(s"Error: $code: $msg")
-                    complete(InternalServerError)
                 }
-                case Failure(e) =>
-                  // The call to the ElasticSearch API failed
-                  System.out.println(e)
-                  complete(InternalServerError)
               }
             }
-          }
+          )
         }
       )
     }
+  }
 }
