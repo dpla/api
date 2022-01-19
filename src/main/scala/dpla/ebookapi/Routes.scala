b3ceece5d6eb9678a7ba07b6ce10649af2333d16
AudreyAltman <audreykaltman@gmail.com>
2022-01-19 16:20:32 -0500

    ebooks controller

--- src/main/scala/dpla/ebookapi/Routes.scala
+++ src/main/scala/dpla/ebookapi/Routes.scala
@@ -1,25 +1,18 @@
 package dpla.ebookapi
 
 import akka.actor.typed.ActorSystem
-import akka.http.scaladsl.model.HttpResponse
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.util.Timeout
-import dpla.ebookapi.v1.ebooks.{ElasticSearchClient, ParamValidator, RawParams, ValidationException}
-import dpla.ebookapi.v1.ebooks.JsonFormats._
-
-import scala.concurrent.ExecutionContextExecutor
-import scala.util.{Failure, Success}
+import dpla.ebookapi.v1.ebooks.{EbooksController, ElasticSearchClient, RawParams}
 
 class Routes(elasticSearchClient: ElasticSearchClient)(implicit val system: ActorSystem[_]) {
 
   // If ask takes more time than this to complete the request is failed
   private implicit val timeout: Timeout = Timeout.create(system.settings.config.getDuration("my-app.routes.ask-timeout"))
 
-  // needed for the future map/onComplete
-  implicit val executionContext: ExecutionContextExecutor = system.executionContext
+  val ebooksController = new EbooksController(elasticSearchClient)
 
   val applicationRoutes: Route = {
     concat(
@@ -89,70 +82,14 @@ class Routes(elasticSearchClient: ElasticSearchClient)(implicit val system: Acto
                         title = title
                       )
 
-                      ParamValidator.getSearchParams(rawParams) match {
-                        case Success(validParams) =>
-                          onComplete(elasticSearchClient.search(validParams)) {
-                            case Success(response) => response match {
-                              case Right(pubsFuture) =>
-                                onComplete(pubsFuture) {
-                                  case Success(pubs) =>
-                                    complete(pubs)
-                                  case Failure(e) =>
-                                    // Failure to parse ElasticSearch response
-                                    System.out.println(s"Error: $e")
-                                    complete(InternalServerError)
-                                }
-                              case Left(status) =>
-                                // ElasticSearch returned an error
-                                val code = status.value
-                                val msg = status.reason
-                                System.out.println(s"Error: $code: $msg")
-                                complete(InternalServerError)
-                            }
-                            case Failure(e) =>
-                              // The call to the ElasticSearch API failed
-                              System.out.println(e)
-                              complete(InternalServerError)
-                          }
-                        case Failure(e: ValidationException) =>
-                          // The user submitted invalid parameters
-                          System.out.println(e.getMessage)
-                          complete(HttpResponse(BadRequest, entity = e.getMessage))
-                        case Failure(e) =>
-                          // This shouldn't happen
-                          System.out.println(e.getMessage)
-                          complete(HttpResponse(InternalServerError, entity = e.getMessage))
-                      }
+                      ebooksController.search(rawParams)
                   }
                 }
               },
               path(Segment) { id =>
                 get {
                   rejectEmptyResponse {
-
-                    onComplete(elasticSearchClient.fetch(id)) {
-                      case Success(response) => response match {
-                        case Right(pubFuture) =>
-                          onComplete(pubFuture) {
-                            case Success(pub) =>
-                              complete(pub)
-                            case Failure(e) =>
-                              // Failure to parse ElasticSearch response
-                              System.out.println(s"Error: $e")
-                              complete(InternalServerError)
-                          }
-                        case Left(status) =>
-                          // ElasticSearch returned an error
-                          val code = status.value
-                          val msg = status.reason
-                          System.out.println(s"Error: $code: $msg")
-                          complete(InternalServerError)
-                      }
-                      case Failure(e) =>
-                        // The call to the ElasticSearch API failed
-                        System.out.println(e)
-                        complete(InternalServerError)
-                    }
+                    ebooksController.fetch(id)
                   }
                 }
               }
