31e9244d081bb383c8c5a2e0a49b2da60219f2ab
AudreyAltman <audreykaltman@gmail.com>
2022-01-20 10:39:07 -0500

    add routes without version

--- src/main/scala/dpla/ebookapi/Routes.scala
+++ src/main/scala/dpla/ebookapi/Routes.scala
@@ -14,96 +14,94 @@ class Routes(elasticSearchClient: ElasticSearchClient)(implicit val system: Acto
 
   val ebooksController = new EbooksController(elasticSearchClient)
 
-  val applicationRoutes: Route = {
-    concat(
+  lazy val applicationRoutes: Route =
+    concat (
+      pathPrefix("ebooks")(ebooksRoutes),
       pathPrefix("v1") {
         concat(
-          pathPrefix("ebooks") {
-            concat(
-              pathEnd {
-                get {
-                  parameters(
-                    "dataProvider".optional,
-                    "exact_field_match".optional,
-                    "facets".optional,
-                    "facet_size".optional,
-                    "isShownAt".optional,
-                    "object".optional,
-                    "page".optional,
-                    "page_size".optional,
-                    "q".optional,
-                    "sourceResource.creator".optional,
-                    "sourceResource.date.displayDate".optional,
-                    "sourceResource.description".optional,
-                    "sourceResource.format".optional,
-                    "sourceResource.language.name".optional,
-                    "sourceResource.publisher".optional,
-                    "sourceResource.subject.name".optional,
-                    "sourceResource.subtitle".optional,
-                    "sourceResource.title".optional) {
-
-                    (dataProvider,
-                     exactFieldMatch,
-                     facets,
-                     facetSize,
-                     isShownAt,
-                     `object`,
-                     page,
-                     pageSize,
-                     q,
-                     creator,
-                     date,
-                     description,
-                     format,
-                     language,
-                     publisher,
-                     subject,
-                     subtitle,
-                     title) =>
-
-                      val rawParams = RawParams(
-                        dataProvider = dataProvider,
-                        exactFieldMatch = exactFieldMatch,
-                        facets = facets,
-                        facetSize = facetSize,
-                        isShownAt = isShownAt,
-                        `object` = `object`,
-                        page = page,
-                        pageSize = pageSize,
-                        q = q,
-                        creator = creator,
-                        date = date,
-                        description = description,
-                        format = format,
-                        language = language,
-                        publisher = publisher,
-                        subject = subject,
-                        subtitle = subtitle,
-                        title = title
-                      )
-
-                      ebooksController.search(rawParams)
-                  }
-                }
-              },
-              path(Segment) { id =>
-                get {
-                  ebooksController.fetch(id)
-                }
-              }
-            )
-          }
+          pathPrefix("ebooks")(ebooksRoutes)
         )
       },
-      pathPrefix("health-check") {
-        concat(
-          pathEnd {
-            get {
-              complete(OK)
-            }
+      path("health-check")(healthCheckRoute)
+    )
+
+  lazy val ebooksRoutes: Route =
+    concat(
+      pathEnd {
+        get {
+          parameters(
+            "dataProvider".optional,
+            "exact_field_match".optional,
+            "facets".optional,
+            "facet_size".optional,
+            "isShownAt".optional,
+            "object".optional,
+            "page".optional,
+            "page_size".optional,
+            "q".optional,
+            "sourceResource.creator".optional,
+            "sourceResource.date.displayDate".optional,
+            "sourceResource.description".optional,
+            "sourceResource.format".optional,
+            "sourceResource.language.name".optional,
+            "sourceResource.publisher".optional,
+            "sourceResource.subject.name".optional,
+            "sourceResource.subtitle".optional,
+            "sourceResource.title".optional) {
+
+            (dataProvider,
+             exactFieldMatch,
+             facets,
+             facetSize,
+             isShownAt,
+             `object`,
+             page,
+             pageSize,
+             q,
+             creator,
+             date,
+             description,
+             format,
+             language,
+             publisher,
+             subject,
+             subtitle,
+             title) =>
+
+              val rawParams = RawParams(
+                dataProvider = dataProvider,
+                exactFieldMatch = exactFieldMatch,
+                facets = facets,
+                facetSize = facetSize,
+                isShownAt = isShownAt,
+                `object` = `object`,
+                page = page,
+                pageSize = pageSize,
+                q = q,
+                creator = creator,
+                date = date,
+                description = description,
+                format = format,
+                language = language,
+                publisher = publisher,
+                subject = subject,
+                subtitle = subtitle,
+                title = title
+              )
+
+              ebooksController.search(rawParams)
           }
-        )
+        }
+      },
+      path(Segment) { id =>
+        get {
+          ebooksController.fetch(id)
+        }
       }
     )
-  }
+
+  lazy val healthCheckRoute: Route =
+      get {
+        complete(OK)
+      }
 }
