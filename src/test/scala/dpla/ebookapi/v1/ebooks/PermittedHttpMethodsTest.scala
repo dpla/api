c26680150128fbe63cb853b2e07dcbc78d99126c
AudreyAltman <audreykaltman@gmail.com>
2022-01-20 18:19:18 -0500

    use mock

--- src/test/scala/dpla/ebookapi/v1/ebooks/PermittedHttpMethodsTest.scala
+++ src/test/scala/dpla/ebookapi/v1/ebooks/PermittedHttpMethodsTest.scala
@@ -6,16 +6,17 @@ import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import dpla.ebookapi.Routes
+import dpla.ebookapi.helpers.Mocks
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-class PermittedHttpMethodsTest extends AnyWordSpec with Matchers with ScalatestRouteTest {
+class PermittedHttpMethodsTest extends AnyWordSpec with Matchers with ScalatestRouteTest with Mocks {
 
   lazy val testKit: ActorTestKit = ActorTestKit()
   implicit def typedSystem: ActorSystem[Nothing] = testKit.system
   override def createActorSystem(): akka.actor.ActorSystem = testKit.system.classicSystem
 
-  val elasticSearchClient = new ElasticSearchClient("http://es-endpoint.com")
+  val elasticSearchClient: ElasticSearchClient = getMockElasticSearchClient
   lazy val routes: Route = new Routes(elasticSearchClient).applicationRoutes
 
   "/v1/ebooks route" should {
