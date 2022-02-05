cac1fc0295c033c0beeb0e872e16f8efa4baa7b9
AudreyAltman <audreykaltman@gmail.com>
2022-02-04 23:21:36 -0500

    fix PermittedHttpMethodsTest

--- src/test/scala/dpla/ebookapi/v1/ebooks/PermittedHttpMethodsTest.scala
+++ src/test/scala/dpla/ebookapi/v1/ebooks/PermittedHttpMethodsTest.scala
@@ -1,91 +1,94 @@
-//package dpla.ebookapi.v1.ebooks
-//
-//import akka.actor.testkit.typed.scaladsl.ActorTestKit
-//import akka.actor.typed.{ActorRef, ActorSystem}
-//import akka.http.scaladsl.model.StatusCodes
-//import akka.http.scaladsl.server.Route
-//import akka.http.scaladsl.testkit.ScalatestRouteTest
-//import dpla.ebookapi.Routes
-//import org.scalatest.matchers.should.Matchers
-//import org.scalatest.wordspec.AnyWordSpec
-//
-//class PermittedHttpMethodsTest extends AnyWordSpec with Matchers with ScalatestRouteTest {
-//
-//  lazy val testKit: ActorTestKit = ActorTestKit()
-//  implicit def typedSystem: ActorSystem[Nothing] = testKit.system
-//  override def createActorSystem(): akka.actor.ActorSystem = testKit.system.classicSystem
-//  val ebookRegistry: ActorRef[EbookRegistry.RegistryCommand] = testKit.spawn(EbookRegistry())
-//  lazy val routes: Route = new Routes(ebookRegistry).applicationRoutes
-//
-//  "/v1/ebooks route" should {
-//    "handle invalid HTTP methods" should {
-//      "reject DELETE" in {
-//        val request = Delete("/v1/ebooks")
-//
-//        request ~> Route.seal(routes) ~> check {
-//          status shouldEqual StatusCodes.MethodNotAllowed
-//        }
-//      }
-//
-//      "reject PATCH" in {
-//        val request = Patch("/v1/ebooks")
-//
-//        request ~> Route.seal(routes) ~> check {
-//          status shouldEqual StatusCodes.MethodNotAllowed
-//        }
-//      }
-//
-//      "reject POST" in {
-//        val request = Post("/v1/ebooks")
-//
-//        request ~> Route.seal(routes) ~> check {
-//          status shouldEqual StatusCodes.MethodNotAllowed
-//        }
-//      }
-//
-//      "reject PUT" in {
-//        val request = Put("/v1/ebooks")
-//
-//        request ~> Route.seal(routes) ~> check {
-//          status shouldEqual StatusCodes.MethodNotAllowed
-//        }
-//      }
-//    }
-//  }
-//
-//  "/v1/ebooks/[id] route" should {
-//    "handle invalid HTTP methods" should {
-//      "reject DELETE" in {
-//        val request = Delete("/v1/ebooks/R0VfVX4BfY91SSpFGqxt")
-//
-//        request ~> Route.seal(routes) ~> check {
-//          status shouldEqual StatusCodes.MethodNotAllowed
-//        }
-//      }
-//
-//      "reject PATCH" in {
-//        val request = Patch("/v1/ebooks/R0VfVX4BfY91SSpFGqxt")
-//
-//        request ~> Route.seal(routes) ~> check {
-//          status shouldEqual StatusCodes.MethodNotAllowed
-//        }
-//      }
-//
-//      "reject POST" in {
-//        val request = Post("/v1/ebooks/R0VfVX4BfY91SSpFGqxt")
-//
-//        request ~> Route.seal(routes) ~> check {
-//          status shouldEqual StatusCodes.MethodNotAllowed
-//        }
-//      }
-//
-//      "reject PUT" in {
-//        val request = Put("/v1/ebooks/R0VfVX4BfY91SSpFGqxt")
-//
-//        request ~> Route.seal(routes) ~> check {
-//          status shouldEqual StatusCodes.MethodNotAllowed
-//        }
-//      }
-//    }
-//  }
-//}
+package dpla.ebookapi.v1.ebooks
+
+import akka.actor.testkit.typed.scaladsl.ActorTestKit
+import akka.actor.typed.{ActorRef, ActorSystem}
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import dpla.ebookapi.Routes
+import dpla.ebookapi.mocks.MockEsClientSuccess
+import dpla.ebookapi.v1.ebooks.ElasticSearchClient.EsClientCommand
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class PermittedHttpMethodsTest extends AnyWordSpec with Matchers with ScalatestRouteTest {
+
+  lazy val testKit: ActorTestKit = ActorTestKit()
+  implicit def typedSystem: ActorSystem[Nothing] = testKit.system
+  override def createActorSystem(): akka.actor.ActorSystem = testKit.system.classicSystem
+  val ebookRegistry: ActorRef[EbookRegistry.RegistryCommand] = testKit.spawn(EbookRegistry())
+  val elasticSearchClient: ActorRef[EsClientCommand] = testKit.spawn(MockEsClientSuccess())
+  lazy val routes: Route = new Routes(ebookRegistry, elasticSearchClient).applicationRoutes
+
+  "/v1/ebooks route" should {
+    "handle invalid HTTP methods" should {
+      "reject DELETE" in {
+        val request = Delete("/v1/ebooks")
+
+        request ~> Route.seal(routes) ~> check {
+          status shouldEqual StatusCodes.MethodNotAllowed
+        }
+      }
+
+      "reject PATCH" in {
+        val request = Patch("/v1/ebooks")
+
+        request ~> Route.seal(routes) ~> check {
+          status shouldEqual StatusCodes.MethodNotAllowed
+        }
+      }
+
+      "reject POST" in {
+        val request = Post("/v1/ebooks")
+
+        request ~> Route.seal(routes) ~> check {
+          status shouldEqual StatusCodes.MethodNotAllowed
+        }
+      }
+
+      "reject PUT" in {
+        val request = Put("/v1/ebooks")
+
+        request ~> Route.seal(routes) ~> check {
+          status shouldEqual StatusCodes.MethodNotAllowed
+        }
+      }
+    }
+  }
+
+  "/v1/ebooks/[id] route" should {
+    "handle invalid HTTP methods" should {
+      "reject DELETE" in {
+        val request = Delete("/v1/ebooks/R0VfVX4BfY91SSpFGqxt")
+
+        request ~> Route.seal(routes) ~> check {
+          status shouldEqual StatusCodes.MethodNotAllowed
+        }
+      }
+
+      "reject PATCH" in {
+        val request = Patch("/v1/ebooks/R0VfVX4BfY91SSpFGqxt")
+
+        request ~> Route.seal(routes) ~> check {
+          status shouldEqual StatusCodes.MethodNotAllowed
+        }
+      }
+
+      "reject POST" in {
+        val request = Post("/v1/ebooks/R0VfVX4BfY91SSpFGqxt")
+
+        request ~> Route.seal(routes) ~> check {
+          status shouldEqual StatusCodes.MethodNotAllowed
+        }
+      }
+
+      "reject PUT" in {
+        val request = Put("/v1/ebooks/R0VfVX4BfY91SSpFGqxt")
+
+        request ~> Route.seal(routes) ~> check {
+          status shouldEqual StatusCodes.MethodNotAllowed
+        }
+      }
+    }
+  }
+}
