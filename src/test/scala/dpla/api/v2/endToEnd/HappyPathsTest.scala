394a865f4324fa25d5c3b33e32e835b95417d6c1
AudreyAltman <audreykaltman@gmail.com>
2022-05-03 09:32:17 -0400

    dpla map ebook list json test

--- src/test/scala/dpla/api/v2/endToEnd/HappyPathsTest.scala
+++ src/test/scala/dpla/api/v2/endToEnd/HappyPathsTest.scala
@@ -69,16 +69,8 @@ class HappyPathsTest extends AnyWordSpec with Matchers with ScalatestRouteTest
         limit should === (Some(100))
 
         val expected = Seq(
-          "ufwPJ34BjqMaVWqX9KZL",
-          "uvwPJ34BjqMaVWqX9KZL",
-          "uqwPJ34BjqMaVWqX9KZZ",
-          "vPwPJ34BjqMaVWqX9KZZ",
-          "vfwPJ34BjqMaVWqX9KZZ",
-          "vvwPJ34BjqMaVWqX9KZZ",
-          "vqwPJ34BjqMaVWqX9Kac",
-          "wPwPJ34BjqMaVWqX9Kac",
-          "wfwPJ34BjqMaVWqX9Kac",
-          "wvwPJ34BjqMaVWqX9Kac"
+          "3dbbf125aba2642e21f17c955bef4e96",
+          "ccde9a5246356b1048873f9d9c71e5bb"
         )
         val ids = readObjectArray(entity, "docs")
           .flatMap(readString(_, "id"))
@@ -105,16 +97,8 @@ class HappyPathsTest extends AnyWordSpec with Matchers with ScalatestRouteTest
 
     "be happy with valid multiple IDs and successful es response" in {
       val idSeq = Seq(
-        "ufwPJ34BjqMaVWqX9KZL",
-        "uvwPJ34BjqMaVWqX9KZL",
-        "uqwPJ34BjqMaVWqX9KZZ",
-        "vPwPJ34BjqMaVWqX9KZZ",
-        "vfwPJ34BjqMaVWqX9KZZ",
-        "vvwPJ34BjqMaVWqX9KZZ",
-        "vqwPJ34BjqMaVWqX9Kac",
-        "wPwPJ34BjqMaVWqX9Kac",
-        "wfwPJ34BjqMaVWqX9Kac",
-        "wvwPJ34BjqMaVWqX9Kac"
+        "3dbbf125aba2642e21f17c955bef4e96",
+        "ccde9a5246356b1048873f9d9c71e5bb"
       )
       val ids = idSeq.mkString(",")
       val request = Get(s"/v2/ebooks/$ids?api_key=$fakeApiKey")
