150aa16037eb4922f6e09d74185004876e1f4b8b
AudreyAltman <audreykaltman@gmail.com>
2022-05-03 14:31:51 -0400

    fix ebook list mapping test

--- src/test/scala/dpla/api/v2/search/EbookListMappingTest.scala
+++ src/test/scala/dpla/api/v2/search/EbookListMappingTest.scala
@@ -11,79 +11,71 @@ class EbookListMappingTest extends AnyWordSpec with Matchers
 
   // TODO rewrite with new elastic search format
 
-//  val esEbookList: String =
-//    readFile("/elasticSearchEbookList.json")
-//  val ebookList: JsObject =
-//    esEbookList.parseJson.convertTo[EbookList].toJson.asJsObject
-//
-//  val minEsEbookList: String =
-//    readFile("/elasticSearchMinimalEbookList.json")
-//  val minEbookList: JsObject =
-//    minEsEbookList.parseJson.convertTo[EbookList].toJson.asJsObject
-//
-//  "a list of ebook records" should {
-//    "map count" in {
-//      val expected = 590
-//      val traversed = readInt(ebookList, "count").getOrElse("NOT FOUND")
-//      assert(traversed == expected)
-//    }
-//
-//    "handle empty docs" in {
-//      val traversed = readObjectArray(minEbookList, "docs")
-//      assert(traversed.isEmpty)
-//    }
-//
-//    "map all docs" in {
-//      val expected = 10
-//      val docCount = readObjectArray(ebookList, "docs").size
-//      assert(docCount == expected)
-//    }
-//
-//    "map doc fields" in {
-//      val expected = Seq(
-//        "ufwPJ34BjqMaVWqX9KZL",
-//        "uvwPJ34BjqMaVWqX9KZL",
-//        "uqwPJ34BjqMaVWqX9KZZ",
-//        "vPwPJ34BjqMaVWqX9KZZ",
-//        "vfwPJ34BjqMaVWqX9KZZ",
-//        "vvwPJ34BjqMaVWqX9KZZ",
-//        "vqwPJ34BjqMaVWqX9Kac",
-//        "wPwPJ34BjqMaVWqX9Kac",
-//        "wfwPJ34BjqMaVWqX9Kac",
-//        "wvwPJ34BjqMaVWqX9Kac"
-//      )
-//      val ids = readObjectArray(ebookList, "docs")
-//        .flatMap(readString(_, "id"))
-//      ids should contain allElementsOf expected
-//    }
-//
-//    "handle empty facets" in {
-//      val parent = readObject(minEbookList)
-//      val children = parent.get.fields.keys
-//      children should not contain "facets"
-//    }
-//
-//    "map all facets" in {
-//      val expected = Seq("provider.@id", "sourceResource.creator")
-//      val parent = readObject(ebookList, "facets")
-//      val children = parent.get.fields.keys
-//      children should contain allElementsOf expected
-//    }
-//
-//    "map facet terms" in {
-//      val expected = Some("http://standardebooks.org")
-//      val firstTerm =
-//        readObjectArray(ebookList, "facets", "provider.@id", "terms").head
-//      val traversed = readString(firstTerm, "term")
-//      assert(traversed == expected)
-//    }
-//
-//    "map facet counts" in {
-//      val expected = Some(590)
-//      val firstTerm =
-//        readObjectArray(ebookList, "facets", "provider.@id", "terms").head
-//      val traversed = readInt(firstTerm, "count")
-//      assert(traversed == expected)
-//    }
-//  }
+  val esEbookList: String =
+    readFile("/elasticSearchEbookList.json")
+  val ebookList: JsObject =
+    esEbookList.parseJson.convertTo[DPLADocList].toJson.asJsObject
+
+  val minEsEbookList: String =
+    readFile("/elasticSearchMinimalEbookList.json")
+  val minEbookList: JsObject =
+    minEsEbookList.parseJson.convertTo[DPLADocList].toJson.asJsObject
+
+  "a list of ebook records" should {
+    "map count" in {
+      val expected = 248887
+      val traversed = readInt(ebookList, "count").getOrElse("NOT FOUND")
+      assert(traversed == expected)
+    }
+
+    "handle empty docs" in {
+      val traversed = readObjectArray(minEbookList, "docs")
+      assert(traversed.isEmpty)
+    }
+
+    "map all docs" in {
+      val expected = 2
+      val docCount = readObjectArray(ebookList, "docs").size
+      assert(docCount == expected)
+    }
+
+    "map doc fields" in {
+      val expected = Seq(
+        "3dbbf125aba2642e21f17c955bef4e96",
+        "ccde9a5246356b1048873f9d9c71e5bb"
+      )
+      val ids = readObjectArray(ebookList, "docs")
+        .flatMap(readString(_, "id"))
+      ids should contain allElementsOf expected
+    }
+
+    "handle empty facets" in {
+      val parent = readObject(minEbookList)
+      val children = parent.get.fields.keys
+      children should not contain "facets"
+    }
+
+    "map all facets" in {
+      val expected = Seq("provider.@id", "sourceResource.creator")
+      val parent = readObject(ebookList, "facets")
+      val children = parent.get.fields.keys
+      children should contain allElementsOf expected
+    }
+
+    "map facet terms" in {
+      val expected = Some("http://dp.la/api/contributor/gpo")
+      val firstTerm =
+        readObjectArray(ebookList, "facets", "provider.@id", "terms").head
+      val traversed = readString(firstTerm, "term")
+      assert(traversed == expected)
+    }
+
+    "map facet counts" in {
+      val expected = Some(248887)
+      val firstTerm =
+        readObjectArray(ebookList, "facets", "provider.@id", "terms").head
+      val traversed = readInt(firstTerm, "count")
+      assert(traversed == expected)
+    }
+  }
 }
