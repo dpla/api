e9facb6a46ba54f823311ae250ab2f9e82b66bd0
AudreyAltman <audreykaltman@gmail.com>
2022-10-06 14:22:25 -0400

    one facet test

--- src/it/scala/dpla/api/v2/search/FacetableFields.scala
+++ src/it/scala/dpla/api/v2/search/FacetableFields.scala
@@ -6,6 +6,7 @@ import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import dpla.api.Routes
+import dpla.api.helpers.ITUtils.fakeApiKey
 import dpla.api.v2.analytics.AnalyticsClient
 import dpla.api.v2.analytics.AnalyticsClient.AnalyticsClientCommand
 import dpla.api.v2.authentication.AuthProtocol.AuthenticationCommand
@@ -47,6 +48,14 @@ class FacetableFields extends AnyWordSpec with Matchers with ScalatestRouteTest
   lazy val routes: Route =
     new Routes(ebookRegistry, itemRegistry, apiKeyRegistry).applicationRoutes
 
+  "Facet by field" should {
+    "return OK" in {
+      val request = Get(s"/v2/items?api_key=$fakeApiKey&page_size=0&facets=dataProvider")
 
+      request ~> routes ~> check {
+        status shouldEqual StatusCodes.OK
+      }
+    }
+  }
 
 }
