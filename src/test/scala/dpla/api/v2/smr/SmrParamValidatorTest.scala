9a9165f40c2bf9eb90f2c7ce1a332245fb79a654
AudreyAltman <audreykaltman@gmail.com>
2023-01-11 12:20:21 -0500

    add param validation tests

--- src/test/scala/dpla/api/v2/smr/SmrParamValidatorTest.scala
+++ src/test/scala/dpla/api/v2/smr/SmrParamValidatorTest.scala
@@ -24,11 +24,112 @@ class SmrParamValidatorTest extends AnyWordSpec with Matchers
   val smrParamValidator: ActorRef[IntermediateSmrResult] =
     testKit.spawn(SmrParamValidator(interProbe.ref))
 
+  val defaultService = "tiktok"
+  val defaultPost = "123"
+  val defaultUser = "abc"
+
   "smr param validator" should {
-    "reject unrecognized params" in {
-      val params = Map("foo" -> "bar")
+    "require service" in {
+      val params = Map("post" -> defaultPost, "user" -> defaultUser)
       smrParamValidator ! RawParams(params, replyProbe.ref)
       replyProbe.expectMessageType[InvalidSmrParams]
+    }
+
+    "require post" in {
+      val params = Map("service" -> defaultService, "user" -> defaultUser)
+      smrParamValidator ! RawParams(params, replyProbe.ref)
+      replyProbe.expectMessageType[InvalidSmrParams]
+    }
+
+    "require user" in {
+      val params = Map("service" -> defaultService, "post" -> defaultPost)
+      smrParamValidator ! RawParams(params, replyProbe.ref)
+      replyProbe.expectMessageType[InvalidSmrParams]
+    }
+
+    "reject unrecognized params" in {
+      val params = Map(
+        "service" -> defaultService,
+        "post" -> defaultPost,
+        "user" -> defaultUser,
+        "foo" -> "bar"
+      )
+      smrParamValidator ! RawParams(params, replyProbe.ref)
+      replyProbe.expectMessageType[InvalidSmrParams]
+    }
+
+    "accept valid service" in {
+      val params = Map(
+        "service" -> defaultService,
+        "post" -> defaultPost,
+        "user" -> defaultUser
+      )
+      smrParamValidator ! RawParams(params, replyProbe.ref)
+      val msg = interProbe.expectMessageType[ValidParams]
+      msg.params.service should be(defaultService)
+    }
+
+    "reject invalid service" in {
+      val params = Map(
+        "service" -> "foo",
+        "post" -> defaultPost,
+        "user" -> defaultUser
+      )
+      smrParamValidator ! RawParams(params, replyProbe.ref)
+      replyProbe.expectMessageType[InvalidSmrParams]
+    }
+
+    "accept valid post" in {
+      val params = Map(
+        "service" -> defaultService,
+        "post" -> defaultPost,
+        "user" -> defaultUser
+      )
+      smrParamValidator ! RawParams(params, replyProbe.ref)
+      val msg = interProbe.expectMessageType[ValidParams]
+      msg.params.post should be(defaultPost)
+    }
+
+    "reject invalid post" in {
+      val params = Map(
+        "service" -> defaultService,
+        "post" -> "<blah>",
+        "user" -> defaultUser
+      )
+      smrParamValidator ! RawParams(params, replyProbe.ref)
+      replyProbe.expectMessageType[InvalidSmrParams]
+    }
+
+    "accept valid user" in {
+      val params = Map(
+        "service" -> defaultService,
+        "post" -> defaultPost,
+        "user" -> defaultUser
+      )
+      smrParamValidator ! RawParams(params, replyProbe.ref)
+      val msg = interProbe.expectMessageType[ValidParams]
+      msg.params.user should be(defaultUser)
+    }
+
+    "reject invalid user" in {
+      val params = Map(
+        "service" -> defaultService,
+        "post" -> defaultPost,
+        "user" -> "bad()=user"
+      )
+      smrParamValidator ! RawParams(params, replyProbe.ref)
+      replyProbe.expectMessageType[InvalidSmrParams]
+    }
+
+    "generate timestamp" in {
+      val params = Map(
+        "service" -> defaultService,
+        "post" -> defaultPost,
+        "user" -> defaultUser
+      )
+      smrParamValidator ! RawParams(params, replyProbe.ref)
+      val msg = interProbe.expectMessageType[ValidParams]
+      msg.params.timestamp should not be empty
     }
   }
 }
