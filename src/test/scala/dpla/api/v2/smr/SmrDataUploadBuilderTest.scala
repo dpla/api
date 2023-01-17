6c1b3294699f9da8ec5a9bfa37a958db8f76b105
AudreyAltman <audreykaltman@gmail.com>
2023-01-17 09:21:32 -0500

    smr upload builder test

---
+++ src/test/scala/dpla/api/v2/smr/SmrDataUploadBuilderTest.scala
@@ -0,0 +1,57 @@
+package dpla.api.v2.smr
+
+import akka.actor.testkit.typed.scaladsl.{ActorTestKit, TestProbe}
+import akka.actor.typed.ActorRef
+import akka.http.scaladsl.model.DateTime
+import dpla.api.v2.smr.SmrProtocol._
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+
+class SmrDataUploadBuilderTest extends AnyWordSpec with Matchers
+  with BeforeAndAfterAll {
+
+  lazy val testKit: ActorTestKit = ActorTestKit()
+
+  override def afterAll(): Unit = testKit.shutdownTestKit()
+
+  val replyProbe: TestProbe[SmrResponse] =
+    testKit.createTestProbe[SmrResponse]
+
+  val interProbe: TestProbe[IntermediateSmrResult] =
+    testKit.createTestProbe[IntermediateSmrResult]
+
+  val smrDataUploadBuilder: ActorRef[IntermediateSmrResult] =
+    testKit.spawn(SmrDataUploadBuilder(interProbe.ref))
+
+  val smrParams: SmrParams = SmrParams(
+    service = "ticktok",
+    post = "123",
+    user = "abc",
+    timestamp = DateTime.fromIsoDateTimeString("2022-06-26T02:00:00").get
+  )
+
+  "data upload builder" should {
+
+    "create json data from user params" in {
+      val expected = "{\"post\":\"123\",\"service\":\"ticktok\",\"timestamp\":\"2022-06-26T02:00:00\",\"user\":\"abc\"}"
+      smrDataUploadBuilder ! ValidSmrParams(smrParams, replyProbe.ref)
+      val msg = interProbe.expectMessageType[SmrUpload]
+      msg.data should be(expected)
+    }
+
+    "create file key name" in {
+      val expected = "1656208800000.json"
+      smrDataUploadBuilder ! ValidSmrParams(smrParams, replyProbe.ref)
+      val msg = interProbe.expectMessageType[SmrUpload]
+      msg.key should be(expected)
+    }
+
+    "pass on replyTo" in {
+      smrDataUploadBuilder ! ValidSmrParams(smrParams, replyProbe.ref)
+      val msg = interProbe.expectMessageType[SmrUpload]
+      msg.replyTo should be(replyProbe.ref)
+    }
+  }
+}
