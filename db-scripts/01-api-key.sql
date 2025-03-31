97aa73f6edee1520ea25857b38b09d479eb6f073
Michael Della Bitta <mdellabitta@tutanota.com>
2025-03-31 13:36:42 -0400

    docker compose setup for running project locally with elasticsearch + postgresql

---
+++ db-scripts/01-api-key.sql
@@ -0,0 +1,8 @@
+insert into account (key,
+                     email,
+                     enabled,
+                     staff)
+values ('ool1Phovah1wie7juapheGoo9aNg1quu',
+        'foo@bar.com',
+        true,
+        true);
